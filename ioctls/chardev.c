e307d46caa3284430c5d0ffc444ffddbc9879f3f
marvol0 <murithimarvin09@gmail.com>
2025-01-04 23:46:56 +0300

    feat(init): create character device driver project
    - Set up initial project structure

---
+++ ioctls/chardev.c
@@ -0,0 +1,24 @@
+/*
+ * chardev.c
+ *
+ * Create an input/output character device with ioctl support
+ * Copyright (C) 1998-99 by Ori Pomerantz
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/wrapper.h>
+#include <asm/uaccess.h>
+#include "chardev.h"
+
+/* Device configuration */
+#define DEVICE_NAME     "char_dev"
+#define DEVICE_MAX_LEN  80
+#define SUCCESS         0
+
+/* Device state */
+static int device_open = 0;
+static char device_message[DEVICE_MAX_LEN];
+static char *message_ptr;
+
